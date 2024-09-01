e760fd674803f58417954216630fcf97525454bc
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2024-09-01 13:31:37 +0700

    com

---
+++ Assets/Enemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // GetComponent<Collider2D>().enabled = false;
+            GameManager.Instance.GameOver();
+        }
+    }
+}
